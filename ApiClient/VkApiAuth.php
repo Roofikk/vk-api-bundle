507e473737b2201534d15c901ea24a3681728341
Roofikk <rufik956@gmail.com>
2021-05-18 10:53:43 +0300

    Auth 1.0

--- ApiClient/VkApiAuth.php
+++ ApiClient/VkApiAuth.php
@@ -11,17 +11,26 @@ use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use VK\OAuth\VKOAuth;
+use VK\OAuth\VKOAuthDisplay;
+use VK\OAuth\Scopes\VKOAuthUserScope;
+use VK\OAuth\Scopes\VKOAuthGroupScope;
+use VK\OAuth\VKOAuthResponseType;
 
 class VkApiAuth
 {
     protected $clientId;
     protected string $redirect_uri;
     protected string $client_secret;
+
+    protected HttpClientInterface $client;
     public function __construct($id, string $redirect_uri, $client_secret)
     {
         $this->clientId = $id;
         $this->redirect_uri = $redirect_uri;
         $this->client_secret = $client_secret;
+
+        $this->client = HttpClient::create([]);
     }
 
     public function get_token()
@@ -31,7 +40,18 @@ class VkApiAuth
 
     protected function autorize()
     {
+        $oauth = new VKOAuth();
+        $client_id = 7854063;
+        $redirect_uri = 'http://heimdallr.senlima.ru:30050/main';
+        $display = VKOAuthDisplay::PAGE;
+        $scope = array(VKOAuthUserScope::WALL, VKOAuthUserScope::GROUPS);
+        $state = 'secret_state_code';
 
+        $browser_url = $oauth->getAuthorizeUrl(VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
+
+        $response = $this->client->request('POST', $browser_url);
+
+        var_dump($response);
     }
 
     protected function get_access_token()
