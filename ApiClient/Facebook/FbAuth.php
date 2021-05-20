e15f79fe2199c3ad2eb6af199144a8039712cf6a
Roofikk <rufik956@gmail.com>
2021-05-20 22:21:45 +0300

    Fb 1.01

--- ApiClient/Facebook/FbAuth.php
+++ ApiClient/Facebook/FbAuth.php
@@ -7,5 +7,14 @@ use Facebook\Facebook;
 
 class FbAuth
 {
+    public function authorize($id, $app_secret)
+    {
+        $fb = new Facebook([
+            'app_id' => $id,
+            'app_secret' => $app_secret,
+            'default_graph_version' => 'v2.10',
+        ]);
 
+        return $fb;
+    }
 }
