459666e46e3bed8a417affa714423911807d0922
Roofikk <rufik956@gmail.com>
2021-05-21 01:11:44 +0300

    Vk post with pict mass

--- ApiClient/VK/VkClient.php
+++ ApiClient/VK/VkClient.php
@@ -55,6 +55,7 @@ class VkClient
         $server = $this->getWallUploadServer();
         for ($i = 0; $i <= count($array_files); $i++)
         {
+            $result = array();
             $response = $this->vkClient->getRequest()->upload($server['upload_url'], 'photo', $array_files[$i]);
             var_dump(pathinfo($array_files[$i]));
             $result = $this->vkClient->photos()->saveWallPhoto($this->accessToken, [
