713a486af9c35eb6b29c05354262f16dfc4c8933
Roofikk <rufik956@gmail.com>
2021-05-21 00:52:46 +0300

    Vk post with pict mass

--- ApiClient/VK/VkClient.php
+++ ApiClient/VK/VkClient.php
@@ -63,6 +63,8 @@ class VkClient
                 'photo'  => $response['photo'],
                 'hash'   => $response['hash'],
             ]));
+
+            var_dump($result);
         }
 
         $attachments = "";
