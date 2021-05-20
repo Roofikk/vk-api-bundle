d143db20208a9d558efefc097007825df33ee860
Roofikk <rufik956@gmail.com>
2021-05-21 01:34:45 +0300

    Vk post with pict mass

--- ApiClient/VK/VkClient.php
+++ ApiClient/VK/VkClient.php
@@ -52,11 +52,10 @@ class VkClient
 
     public function wallPostWithPict($group_id, $array_files, $message)
     {
-        $response = [];
         $server = $this->vkClient->photos()->getWallUploadServer($this->accessToken);
-        for ($i = 0; $i <= count($array_files); $i++)
+        for ($i = 0; $i < count($array_files); $i++)
         {
-            $response[$i] = $this->vkClient->getRequest()->upload($server['upload_url'], 'photo', $array_files[0]);
+            $response = $this->vkClient->getRequest()->upload($server['upload_url'], 'photo', $array_files[$i]);
             var_dump($response);
         }
 
