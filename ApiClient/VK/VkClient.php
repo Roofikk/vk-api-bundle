3029158c7ac3d328afbbeac455eb4bc857ed4eeb
Roofikk <rufik956@gmail.com>
2021-05-21 14:27:52 +0300

    Story video 1.03

--- ApiClient/VK/VkClient.php
+++ ApiClient/VK/VkClient.php
@@ -118,7 +118,7 @@ class VkClient
         ]);
         var_dump($storyInfo);
 
-        $address = $this->vkClient->getRequest()->upload($storyInfo['upload_url'], 'video_file', $video);
+        $address = $this->vkClient->getRequest()->upload(string($storyInfo['upload_url']), 'video_file', string($video));
         var_dump($address);
         $response = $this->vkClient->getRequest()->post('stories.save', $this->accessToken, [
             'upload_results' => $address['upload_result'],
