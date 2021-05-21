fdf3711aadbb79e92500daf57e86f915fa7ac65b
Roofikk <rufik956@gmail.com>
2021-05-21 03:13:12 +0300

    VIDEVO

--- ApiClient/VK/VkAuth.php
+++ ApiClient/VK/VkAuth.php
@@ -38,7 +38,7 @@ class VkAuth
         $client_id = $this->clientId;
         $redirect_uri = 'https://oauth.vk.com/blank.html';
         $display = VKOAuthDisplay::PAGE;
-        $scope = array(VKOAuthUserScope::WALL, VKOAuthUserScope::GROUPS, VKOAuthUserScope::PHOTOS);
+        $scope = array(VKOAuthUserScope::WALL, VKOAuthUserScope::GROUPS, VKOAuthUserScope::PHOTOS, VKOAuthUserScope::VIDEO);
         $state = 'secret_state_code';
 
         $browser_url = $this->oauth->getAuthorizeUrl(VKOAuthResponseType::CODE, $client_id, $redirect_uri, $display, $scope, $state);
